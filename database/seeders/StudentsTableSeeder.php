dce5b41410d1413e8ea72e865f61b2aee45b8283
edwin.durango1987 <edwin.durango20987@ucaldas.edu.co>
2023-10-14 18:13:58 -0500

    seeders

--- database/seeders/StudentsTableSeeder.php
+++ database/seeders/StudentsTableSeeder.php
@@ -5,9 +5,7 @@ namespace Database\Seeders;
 //use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\student;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Str;
-use Illuminate\Support\Facades\Hash;
+
 
 
 class StudentsTableSeeder extends Seeder
@@ -17,42 +15,24 @@ class StudentsTableSeeder extends Seeder
      */
     public function run(): void
     {
-        //
-        //student::create(
-            //[
-                //'document_number' => ['1234567890', '1234567891', '1234567892', '1234567893', '1234567894', '1234567895', '1234567896', '1234567897', '1234567898', '1234567899'],
-                //'name' => ['Joaquin','ruben','edwin','jose','maria','luis','julian','juliana','julio','juliana'],
-                //'sex' => ['M','M','M','M','F','M','M','F','M','F'],
-                //'marital_status' => ['S','C','V','D','U','S','C','V','D','U'],
-                //'date_of_birth' => ['1999-01-01','1999-01-02','1999-01-03','1999-01-04','1999-01-05','1999-01-06','1999-01-07','1999-01-08','1999-01-09','1999-01-10'],
-                //'residence_address' => ['Calle 1 # 1-1','Calle 1 # 1-2','Calle 1 # 1-3','Calle 1 # 1-4','Calle 1 # 1-5','Calle 1 # 1-6','Calle 1 # 1-7','Calle 1 # 1-8','Calle 1 # 1-9','Calle 1 # 1-10'],
-                //'stratum' => ['1','2','3','4','5','6','1','2','3','4'],
-                //'residence_type' => ['URBANA','RURAL','URBANA','RURAL','URBANA','RURAL','URBANA','RURAL','URBANA','RURAL'],
-                //'height' => ['170','170','170','170','170','170','170','170','170','170'],
-                //'institutional_email' => ['joaquin@','ruben@','edwin@','jose@','maria@','luis@','julian@','juliana@','julio@','juliana@'],
-                //'cell_phone_number' => ['1234567890','1234567891','1234567892','1234567893','1234567894','1234567895','1234567896','1234567897','1234567898','1234567899']
 
-
-
-            //]
-
-        //);
-
-        DB::table('students')-> insert (
-            [
-                'name' => STR::random(10),
-                'document_number' => STR :: random(10),
-                'sex' => STR :: random(3),
-                'marital_status' => STR :: random(5),
-                'date_of_birth' => STR :: random(10),
-                'residence_address' => STR :: random(10),
-                'stratum' => STR :: random(10),
-                'residence_type' => STR :: random(10),
-                'height' => INT :: random(10),
-                'institutional_email' => STR :: random(10),
-                'cell_phone_number' => INT :: random(10),
+        // Crear 10 estudiantes con datos de ejemplo
+        for ($i = 1; $i <= 10; $i++) {
+            Student::create([
+                'document_Number' => 'Document' . $i,
+                'name' => 'Estudiante ' . $i,
+                'sex' => ($i % 2 == 0) ? 'M' : 'F', // Alternar entre masculino y femenino
+                'marital_status' => ($i % 2 == 0) ? 'C' : 'S', // Alternar entre casado y soltero
+                'date_of_birth' => '1990-05-' . (10 + $i),
+                'residence_address' => 'Calle ' . (100 + $i) . ', Ciudad, País',
+                'stratum' => rand(1, 6), // Generar un valor aleatorio entre 1 y 6 para el estrato
+                'residence_type' => ($i % 2 == 0) ? 'Urbana' : 'Rural', // Alternar entre urbana y rural
+                'height' => rand(150, 190), // Generar una altura aleatoria en cm
+                'institutional_email' => 'estudiante' . $i . '@example.com',
+                'cell_phone_number' => '123-456-789' . $i,
             ]);
 
+        }
 
     }
 }
